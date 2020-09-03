8ba9547408d190cd65f0a61b3da930d4f6d40110
shiyi <1771270587@qq.com>
2020-09-03 14:49:58 +0800

    index over

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,21 @@
 $.ajaxPrefilter(function(options){
     // 在真正发起 Ajax 请求之前，统一拼接请求的根路径
     options.url ='http://ajax.frontend.itheima.net' + options.url
+    // 统一为有权限的接口， 设置headers设置请求头
+    if(options.url.indexOf('/my/') !== -1){
+       options.headers={
+            Authorization:localStorage.getItem('token')
+        }   
+    }
+    //无论成功或者失败，都会执行 complete 函数
+    options.complete=function(res){
+        console.log('执行了complete回调')
+        console.log(res);
+        if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！git '){
+            //1. 强制清空本地 token
+            localStorage.removeItem('token')
+            //2. 强制跳转到本地页面
+            location.href = '/login.html'
+        }
+    }
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -14,7 +14,7 @@ $(function(){
     var form = layui.form
     var layer = layui.layer
     // 添加表达的自定义校验规则
-    console.dir(form)
+    // console.dir(form)
     form.verify({
         pwd:[/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'],
         repwd:function(value){
@@ -38,7 +38,7 @@ $(function(){
             password:$('#form_reg [name=password]').val()
         }
         $.post('/api/reguser',data,function(res){
-            console.log(res);
+            // console.log(res);
             if(res.status !== 0){
                 return layer.msg(res.message)
             }
